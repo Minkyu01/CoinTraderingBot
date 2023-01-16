b400568fd86e7f8d740ff3346a8ec1a95bbe7d5b
Leejhy <jeehyeong0128@gmail.com>
2023-01-17 00:39:48 +0900

    HTML 만 조금함

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/coinback')
+@app.route('/') #나중에 URL coinback으로 수정
 def coin():
     return render_template("coinbacktest.html")
